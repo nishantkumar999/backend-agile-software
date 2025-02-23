0fae8043423ba6c34f5527c977259d3b94aab774
Nishant Kumar <nishantkumar8852@gmail.com>
2025-02-23 10:32:26 +0530

    agile-backend : added initial code

---
+++ agile-backend/src/main/java/com/agileApplication/backend/AgileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agileApplication.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileBackendApplication.class, args);
+	}
+
+}
